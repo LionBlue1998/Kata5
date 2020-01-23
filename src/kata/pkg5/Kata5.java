d54cb1f6cdb4675b8dbc50785b45cb8783d8ade1
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2020-01-23 10:30:46 +0000

    Kata5: Ej2

---
+++ src/kata/pkg5/Kata5.java
@@ -0,0 +1,12 @@
+package kata.pkg5;
+
+import java.sql.SQLException;
+
+public class Kata5 {
+
+    public static void main(String[] args) throws SQLException {
+       SelectApp app = new SelectApp();
+       app.SelectAll();
+    }
+    
+}
